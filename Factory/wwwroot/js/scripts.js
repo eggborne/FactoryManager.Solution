cd312c1de174adf93344e92d5b85230bac52cf19
Mike Donovan <mike@mikedonovan.dev>
2023-03-18 16:52:16 -0700

    Add page transition effect

--- Factory/wwwroot/js/scripts.js
+++ Factory/wwwroot/js/scripts.js
@@ -1,3 +1,7 @@
 window.addEventListener('load', () => {
   document.documentElement.style.setProperty('--actual-height', window.innerHeight + 'px')
+  // document.getElementById('engineer-splash-list').classList.remove('obscured');
+  // document.getElementById('machine-splash-list').classList.remove('obscured');
+  document.querySelector('main').classList.remove('obscured');
+  console.log("window load event fired from <script> in _Layout")
 });
