c501582d2ac90b13d9536e1654a6245e5c55dab9
Mike Donovan <mike@mikedonovan.dev>
2023-03-18 17:27:50 -0700

    Refine transition animation to hide misplaced elements

--- Factory/wwwroot/js/scripts.js
+++ Factory/wwwroot/js/scripts.js
@@ -1,4 +1,13 @@
-window.addEventListener('load', () => {
-  document.documentElement.style.setProperty('--actual-height', window.innerHeight + 'px')
+const pause = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const swapClass = (element, oldClass, newClass) => {
+  if (element.classList.contains(oldClass)) {
+    element.classList.remove(oldClass);
+  }
+  element.classList.add(newClass);
+};
+
+window.addEventListener('load', async () => {
+  document.documentElement.style.setProperty('--actual-height', window.innerHeight + 'px');
   document.querySelector('main').classList.remove('obscured');
 });
