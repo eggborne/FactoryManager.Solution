64f993e9909de258da6e49c94b04d3692f78b617
Mike Donovan <mike@mikedonovan.dev>
2023-03-18 19:31:06 -0700

    Create modals for delete confirmation

--- Factory/wwwroot/js/scripts.js
+++ Factory/wwwroot/js/scripts.js
@@ -11,3 +11,13 @@ window.addEventListener('load', async () => {
   document.documentElement.style.setProperty('--actual-height', window.innerHeight + 'px');
   document.querySelector('main').classList.remove('obscured');
 });
+
+function showModal(type) {
+  document.body.classList.add('veiled');
+  document.getElementById(`${type}-modal`).classList.remove('obscured');
+}
+
+function hideModal(type) {
+  document.body.classList.remove('veiled');
+  document.getElementById(`${type}-modal`).classList.add('obscured');
+}
